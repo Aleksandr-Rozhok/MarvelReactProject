61ff1649441db5f2ca67c7e6a9cee33af34c1bba
Aliaksandr Razhok <aliaksandr.razhok@LeverX.local>
2023-03-22 17:13:25 +0300

    add: propTypes

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import './charList.scss';
 
 import MarvelService from '../../services/MarvelService';
@@ -89,6 +91,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func
 }
 
 export default CharList;
